85b970ccc40638e584ab5d6b73930f9e2a032da1
ymlasu <33670544+ymlasu@users.noreply.github.com>
2018-08-20 08:14:45 -0700

    Version compatibility
    Changes made to the python version and pip package install.

--- dependencies.sh
+++ dependencies.sh
@@ -4,11 +4,12 @@ echo "Installing PARA-ATM dependencies..."
 
 sudo chmod 777 Anaconda3-2.4.0-Linux-x86_64.sh
 ./Anaconda3-2.4.0-Linux-x86_64.sh
+sudo apt-get install python3-pip
 sudo apt-get install postgresql
 sudo apt-get install pgadmin3
 sudo apt-get install postgresql-contrib
 . ~/.bashrc
-pip install psycopg2-binary
-pip install rdflib
+pip3 install psycopg2-binary
+pip3 install rdflib
 
 echo "Done."
