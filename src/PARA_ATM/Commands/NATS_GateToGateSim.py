a07df6b49e0626f646016f0bf5a04e2cd19f8574
Michael Hartnett <michael.hartnett@swri.org>
2018-11-12 13:50:51 -0600

    initial commit

--- src/PARA_ATM/Commands/NATS_GateToGateSim.py
+++ src/PARA_ATM/Commands/NATS_GateToGateSim.py
@@ -30,6 +30,7 @@ class Command:
         pid=os.fork()
         parentPath = str(Path(__file__).parent.parent.parent)
         if pid==0:
+            host_port = 'localhost:2017'
             server_response = os.system('curl -s ' + host_port) >> 8
             if server_response == 52 or server_response == 0:
                 exit()
