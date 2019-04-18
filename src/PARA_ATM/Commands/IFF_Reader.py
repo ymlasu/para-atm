a3f944956d5eef5321a4a8748a040c4841218a92
ed-swri <erin.decarlo@swri.org>
2019-04-18 14:51:22 -0500

    Simplified IFF reader - removed EV data merging

--- src/PARA_ATM/Commands/IFF_Reader.py
+++ src/PARA_ATM/Commands/IFF_Reader.py
@@ -51,40 +51,31 @@ class Command:
         for i in range(len(cols)):
             dfs.append(pd.DataFrame(columns=cols[i]))
                        
-        data = pd.read_csv(open(parentPath + "/../data/Sherlock/" + self.filename),encoding='latin1',low_memory=False,header=None)
+        data = pd.read_csv(open(parentPath + "/../data/Sherlock/" + self.filename,encoding='latin9'),low_memory=False,header=None)
 
         for dfIdx in data[0].unique():
             print(dfIdx)
-            df = data[data[0]==dfIdx].reset_index()
+            df = data[data[0]==dfIdx].reset_index(drop=True)
             nCols = min(len(cols[dfIdx]),len(df.columns))
             df = df[df.columns[0:nCols]]
             df.columns = cols[dfIdx][0:nCols]
             dfs[dfIdx]=df
                                   
         data = dfs[3]
-        ev = pd.read_csv(open(parentPath + "/../data/Sherlock/" + 'EV_'+'_'.join(self.filename.split('_')[1:]),'r'),header=0)
-        ev['time'] = ev['tStartSecs'] + ev['tEv']
-        ev = ev[['time','AcId','EvType']]
-        results = pd.DataFrame()
+
+        data['Orig'] = np.nan
+        data['Dest'] = np.nan
         
         #cycle through header rows
-        last_index = -1
-        for index,row in data[data['recType']==2].iterrows():
-            if last_index < 0:
-                last_index = index
-                continue
-            #track records start after header and flight plan records
-            start = last_index+2
-            end = index
-            #copy origin
-            data.iloc[start:end,3] = data.iloc[last_index,10]
-            #copy destination
-            data.iloc[start:end,4] = data.iloc[last_index,11]
-            ev_types = ev.iloc[np.where(ev['AcId'] == data.iloc[last_index,7])[0]]
-            data.iloc[start:end] = pd.merge(data.iloc[start:end], ev_types, left_on=['recTime','AcId'], right_on=['time','AcId'])
-            print(data.iloc[start:end].columns)
-            results=results.append(data.iloc[start:end][['recTime','AcId','bcnCode','cid','coord1','coord2','alt','rateOfClimb','groundSpeed','course','EvType']])
+        for fp in data.fltKey.unique():
+            idx = data[data.fltKey==fp].index
+            orig = dfs[2][dfs[2].fltKey==fp]['Orig']
+            dest = dfs[2][dfs[2].fltKey==fp]['Dest']
             
-        results.columns = ['time','callsign','origin','destination','latitude','longitude','altitude','rocd','tas','heading','mode']
+            values = {'Orig':orig,'Dest':dest}
+            data.loc[idx,['Orig','Dest']].fillna(value=values,inplace=True)
+                        
+        data=data[['recTime','AcId','Orig','Dest','coord1','coord2','alt','rateOfClimb','groundSpeed','course']]   
+        data.columns = ['time','callsign','origin','destination','latitude','longitude','altitude','rocd','tas','heading','mode']
 
-        return ["IFF_Reader", results]
+        return ["IFF_Reader", data]
