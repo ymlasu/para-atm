f2619e585da81b32f36cc22a1be61f49006d2b58
mh-swri <michael.hartnett@swri.org>
2019-09-19 09:27:13 -0500

    added comments and made run executable

--- src/PARA_ATM/Commands/uncertaintyProp.py
+++ src/PARA_ATM/Commands/uncertaintyProp.py
@@ -20,24 +20,38 @@ class Command:
         should be kept the same (Command).
     '''
     
-    #Here, the database connector and the parameter are passed as arguments. This can be changed as per need.
     def __init__(self,safety_module):
+        """
+            uncertaintyProp command propagates uncertainty from given sources through a given safety metric
+            args:
+                safety_module (str) : the module to calculate the safety metric and its parameters
+                n_samples (int) : the number of samples required from the distribution(s)
+        """
+        #parse the safety_module string
         self.mod_name = safety_module.split('(')[0]
         self.in_file = safety_module.split('(')[1][:-1]
+        #load the module
         self.safety_module = getattr(PARA_ATM.Commands,self.mod_name)
-        self.n_samples = 1
+
+        #future args for uncertaintyProp
+        self.n_samples = 1000
         self.uncertainty_sources = ['atc','pilot','vehicle']
         self.states = ['nominal']
 
     #Method name executeCommand() should not be changed. It executes the query and displays/returns the output.
     def executeCommand(self):
+        """
+            returns:
+                results (list) : [moduleName, aggregated return from the safety module
+        """
 
+        #instantiate helper function module for db access
         db_access = DataStore.Access()
 
+        #get Centaur distribution objects from the database
         dist_objs = [db_access.getCentaurDist(subject,state) for subject in self.uncertainty_sources for state in self.states]
 
-
-        #Create random variable
+        #create random variable matrix
         rv_vector = np.array([np.array(dist.sample(self.n_samples)) for dist in dist_objs]).reshape(-1,3)
 
         #propagate
