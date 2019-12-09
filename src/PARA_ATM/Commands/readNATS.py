49b8a3410e72d08a0d68ad5a770731fa3537b5e5
John McFarland <john.mcfarland@swri.org>
2019-12-09 17:51:34 -0600

    Initial reorganization of NATS file input/output
    The main motivation is to remove the path dependence of the readNATS
    function.  The actual code behind this function has been moved into a
    function read_nats_output_file, within a new module io/nats.py.  The
    new function operates in a standard way by reading the requested file
    without assuming any particular base path.  It also returns only the
    DataFrame, instead of a list with identifier, like readNATS.
    
    Although the Commands structure likely needs to go away, I have left
    it in place for now, and have it simply call read_nats_output_file.
    For now, Commands.readNATS still assumes the source path for
    compatibility with usage from the GUI (it is worth discussing whether
    using the GUI to read files is a real use case).

--- src/PARA_ATM/Commands/readNATS.py
+++ src/PARA_ATM/Commands/readNATS.py
@@ -13,6 +13,9 @@ Visualize NATS output CSV file
 from pathlib import Path
 import pandas as pd
 import numpy as np
+import os
+
+from PARA_ATM.io.nats import read_nats_output_file
 
 class Command:
     """
@@ -38,50 +41,10 @@ class Command:
                 command name to be passed to MapView, etc.
                 results = dataframe of shape (# position records, 12)
         """
-        interp = False
         if self.filename == '':
             return ('readNATS',pd.DataFrame())
-        #src directory
+
         parentPath = str(Path(__file__).parent.parent.parent)
-        #trajectory record rows have different fields than header rows
-        cols = ['time','lat','lon','altitude','rocd','tas','tas_ground','heading','fpa','sect_ind','sect_name','mode','dest_elev','nrows']
-        
-        #skip the initial header of the csv file
-        output = pd.read_csv(open(parentPath + "/NATS/" + self.filename, 'r'),header=None,names=cols,skiprows=9)
-        results = pd.DataFrame()
-        
-        #cycle through header rows
-        for index,row in output[output['nrows'] > 0].iterrows():
-            #rows start immediately after header, end after specified nrows
-            start = index+1
-            end = start+int(row['nrows'])
-            #copy acid to unused column of trajectory rec
-            output.iloc[start:end,-1] = row['lon']
-            #copy origin
-            output.iloc[start:end,-2] = row['rocd']
-            #copy destination
-            output.iloc[start:end,6] = row['tas']
-            results=results.append(output.iloc[start:end][['time','nrows','tas_ground','dest_elev','lat','lon','altitude','rocd','tas','heading','sect_name','mode']])
-            
-        col = ['time','callsign','origin','destination','latitude','longitude','altitude','rocd','tas','heading','sector','status']
-        results.columns = col
-        results['time'] = pd.to_datetime(results['time'].astype(float),unit='s')
-        floats = ['latitude','longitude','altitude','rocd','tas','heading']
-        strs = ['callsign','origin','destination','sector','status']
-        results[floats] = results[floats].astype(float)
-        results[strs] = results[strs].astype(str).fillna('unknown')
-        print(results)
-        if interp and (results.at[2,'time'] - results.at[1,'time']) >= pd.to_timedelta('1s'):
-            temp = pd.DataFrame()
-            results = results.set_index('time')
-            for acid in np.unique(results['callsign']):
-                upsample = results[results['callsign']==acid].resample('ms')
-                interp = upsample.interpolate(method='linear')
-                try:
-                    interp[strs] = interp[strs].interpolate(method='pad')
-                except Exception as e:
-                    print(e)
-                temp = temp.append(interp,ignore_index=True)
-            results = temp.fillna(method='ffill')
+        results = read_nats_output_file(os.path.join(parentPath, 'NATS', self.filename))
 
         return ["readNATS", results]
---
+++ src/PARA_ATM/io/nats.py
@@ -0,0 +1,53 @@
+"""Functions for interfacing to NATS input/output files"""
+
+from pathlib import Path
+import pandas as pd
+import numpy as np
+
+
+def read_nats_output_file(filename, interp=False):
+    """Read the specified NATS output file
+
+    Return corresponding formatted pandas DataFrame"""
+    #trajectory record rows have different fields than header rows
+    cols = ['time','lat','lon','altitude','rocd','tas','tas_ground','heading','fpa','sect_ind','sect_name','mode','dest_elev','nrows']
+
+    #skip the initial header of the csv file
+    output = pd.read_csv(filename, header=None, names=cols, skiprows=9)
+    results = pd.DataFrame()
+
+    #cycle through header rows
+    for index,row in output[output['nrows'] > 0].iterrows():
+        #rows start immediately after header, end after specified nrows
+        start = index+1
+        end = start+int(row['nrows'])
+        #copy acid to unused column of trajectory rec
+        output.iloc[start:end,-1] = row['lon']
+        #copy origin
+        output.iloc[start:end,-2] = row['rocd']
+        #copy destination
+        output.iloc[start:end,6] = row['tas']
+        results=results.append(output.iloc[start:end][['time','nrows','tas_ground','dest_elev','lat','lon','altitude','rocd','tas','heading','sect_name','mode']])
+
+    col = ['time','callsign','origin','destination','latitude','longitude','altitude','rocd','tas','heading','sector','status']
+    results.columns = col
+    results['time'] = pd.to_datetime(results['time'].astype(float),unit='s')
+    floats = ['latitude','longitude','altitude','rocd','tas','heading']
+    strs = ['callsign','origin','destination','sector','status']
+    results[floats] = results[floats].astype(float)
+    results[strs] = results[strs].astype(str).fillna('unknown')
+    print(results)
+    if interp and (results.at[2,'time'] - results.at[1,'time']) >= pd.to_timedelta('1s'):
+        temp = pd.DataFrame()
+        results = results.set_index('time')
+        for acid in np.unique(results['callsign']):
+            upsample = results[results['callsign']==acid].resample('ms')
+            interp = upsample.interpolate(method='linear')
+            try:
+                interp[strs] = interp[strs].interpolate(method='pad')
+            except Exception as e:
+                print(e)
+            temp = temp.append(interp,ignore_index=True)
+        results = temp.fillna(method='ffill')
+
+    return results
