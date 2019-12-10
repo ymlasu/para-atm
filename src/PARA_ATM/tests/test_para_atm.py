dfd3a51a55c2fd0f1d0df141a0f9560e6a8cf1d4
John McFarland <john.mcfarland@swri.org>
2019-12-09 18:10:29 -0600

    Add test for table selection change
    When the table selection changes, the app automatically tries to read
    in the indicated table from the database.  To test this, we first read
    the sample NATS file (now possible via the general
    read_nats_output_file function) and add it to the database as
    'test_table'.  The tables selection is changed to trigger the
    callback.  Lastly, the temporary table is removed.

--- src/PARA_ATM/tests/test_para_atm.py
+++ src/PARA_ATM/tests/test_para_atm.py
@@ -10,8 +10,11 @@ from PARA_ATM.io.nats import read_nats_output_file
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 sample_nats_file = os.path.join(THIS_DIR, '..', 'sample_data/NATS_output_SFO_PHX.csv')
 
-class TestLaunchApp(unittest.TestCase):
-    def test(self):
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.container = LaunchApp.Container()
+    
+    def test_construction(self):
         """Test construction of LaunchApp container"""
         # Verify that the Container constructor runs without error.
         # In future, could add assertions that check specific
@@ -19,7 +22,30 @@ class TestLaunchApp(unittest.TestCase):
         # widgets/tables have certain characteristics), but for now
         # the test is soley used to verify that exceptions do not
         # occur during creation of the container.
-        c = LaunchApp.Container()
+
+        # Although there is no code here, this is kept as a separate
+        # test to differentiate between errors in container creation
+        # that happen in setUp, versus errors in functions tested via
+        # other test cases.
+        pass
+
+    def testTableSelect(self):
+        """Test the callback when the table selection changes"""
+        
+        db_access = Access()
+
+        # First we need to ensure there is a valid table in the
+        # database, so we do this by reading in a sample NATS output
+        # file and adding it to the database
+        df = read_nats_output_file(sample_nats_file)
+        db_access.addTable('test_table', df)
+
+        # Try and select the new table
+        self.container.tables.value = 'test_table'
+
+        # Remove the table
+        db_access.dropTable('test_table')
+        
 
 class TestDB(unittest.TestCase):
     def setUp(self):
