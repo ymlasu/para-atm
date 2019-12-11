78dff5a68697e310a5a8183a06e9684c74ebb695
John McFarland <john.mcfarland@swri.org>
2019-12-11 15:45:03 -0600

    Rewrite of code to read NATS output file
    New approach uses the actual column names (for both header and data
    rows) from the NATS output file, instead of making assumptions about
    their positions.  Also add the start time offset, which was not being
    done before.
    
    Dropping the interpolation code for now.  Suspect that this would be
    better served through a separate utility function to reduce code
    duplication.

--- src/PARA_ATM/io/nats.py
+++ src/PARA_ATM/io/nats.py
@@ -1,52 +1,66 @@
 """Functions for interfacing to NATS input/output files"""
 
-from pathlib import Path
 import pandas as pd
 import numpy as np
+import linecache
+from io import StringIO
 
 
-def read_nats_output_file(filename, interp=False):
+def read_nats_output_file(filename):
     """Read the specified NATS output file
 
-    Return corresponding formatted pandas DataFrame"""
-    #trajectory record rows have different fields than header rows
-    cols = ['time','lat','lon','altitude','rocd','tas','tas_ground','heading','fpa','sect_ind','sect_name','mode','dest_elev','nrows']
+    Parameters
+    ----------
+    filename : str
+        Input file to read
 
-    #skip the initial header of the csv file
-    output = pd.read_csv(filename, header=None, names=cols, skiprows=9)
-    results = pd.DataFrame()
+    Returns
+    -------
+    Single formatted data frame"""
 
-    #cycle through header rows
-    for index,row in output[output['nrows'] > 0].iterrows():
-        #rows start immediately after header, end after specified nrows
-        start = index+1
-        end = start+int(row['nrows'])
-        #copy acid to unused column of trajectory rec
-        output.iloc[start:end,-1] = row['lon']
-        #copy origin
-        output.iloc[start:end,-2] = row['rocd']
-        #copy destination
-        output.iloc[start:end,6] = row['tas']
-        results=results.append(output.iloc[start:end][['time','nrows','tas_ground','dest_elev','lat','lon','altitude','rocd','tas','heading','sect_name','mode']])
+    # Read header information out of specific line numbers
+    header_cols = linecache.getline(filename, 5).strip()[3:].split(',')
+    data_cols = linecache.getline(filename, 6).strip()[3:].split(',')
+    start_time = int(linecache.getline(filename, 8))
 
-    col = ['time','callsign','origin','destination','latitude','longitude','altitude','rocd','tas','heading','sector','status']
-    results.columns = col
-    results['time'] = pd.to_datetime(results['time'].astype(float),unit='s')
-    floats = ['latitude','longitude','altitude','rocd','tas','heading']
-    strs = ['callsign','origin','destination','sector','status']
-    results[floats] = results[floats].astype(float)
-    results[strs] = results[strs].astype(str).fillna('unknown')
-    if interp and (results.at[2,'time'] - results.at[1,'time']) >= pd.to_timedelta('1s'):
-        temp = pd.DataFrame()
-        results = results.set_index('time')
-        for acid in np.unique(results['callsign']):
-            upsample = results[results['callsign']==acid].resample('ms')
-            interp = upsample.interpolate(method='linear')
-            try:
-                interp[strs] = interp[strs].interpolate(method='pad')
-            except Exception as e:
-                print(e)
-            temp = temp.append(interp,ignore_index=True)
-        results = temp.fillna(method='ffill')
+    # Read all lines after header
+    with open(filename, 'r') as f:
+        lines = f.readlines()[9:]
 
-    return results
+    # Flag header lines, which may occur throughout the file
+    is_header_line = [line.split(',')[0].isalpha() for line in lines]
+    header_indices = np.where(is_header_line)[0]
+
+    # Read in data by iterating over the header rows.  Each header row
+    # specifies the number of records that follow.  That number is
+    # used to find the corresponding subset of data lines associated
+    # with the header, and those lines are read in as a DataFrame
+    # according to the column names defined above.
+    df = pd.DataFrame(columns=data_cols + ['callsign','origin','destination'])
+    for header_idx in header_indices:
+        header_row = pd.read_csv(StringIO(lines[header_idx]), header=None, names=header_cols).iloc[0]
+        nrows = header_row['number_of_trajectory_rec']
+        aircraft_df = pd.read_csv(StringIO('\n'.join(lines[header_idx+1:header_idx+1+nrows])), header=None, names=data_cols)
+        # Fill in auxiliary data that comes from the header row
+        aircraft_df['callsign'] = header_row['callsign']
+        aircraft_df['origin'] = header_row['origin_airport']
+        aircraft_df['destination'] = header_row['destination_airport']
+        df = df.append(aircraft_df)
+
+
+    df.rename(columns={'timestamp(UTC sec)':'time',
+                       'course':'heading',
+                       'rocd_fps':'rocd',
+                       'sector_name':'sector',
+                       'tas_knots':'tas',
+                       'flight_phase':'status',
+                       'altitude_ft':'altitude'},
+              inplace=True)
+    df['time'] += start_time
+    df['time'] = pd.to_datetime(df['time'], unit='s')
+
+    # Keep only selected columns
+    selected_cols = ['time','callsign','origin','destination','latitude','longitude','altitude','rocd','tas','heading','sector','status']
+    df = df[selected_cols]
+
+    return df
