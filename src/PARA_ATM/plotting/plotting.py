039392917881be673ff146b4be6d8a33ff9cb884
John McFarland <john.mcfarland@swri.org>
2019-12-12 17:24:04 -0600

    Add basic function plot_trajectory
    This is a standalone function that uses bokeh to create a static html
    page with a simple trajectory plot.  This is adapated from the code in
    LaunchApp.  The intent is to start building plotting capability into
    an API.

---
+++ src/PARA_ATM/plotting/plotting.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+import bokeh as bk
+import bokeh.plotting as bkplot
+from bokeh.tile_providers import Vendors, get_provider
+
+from PARA_ATM.Application.plotting_tools import merc
+
+
+def plot_trajectory(df_in, output_file=None):
+    """Plot scenario trajectory to static html and open browser
+    
+    Parameters
+    ----------
+    df_in : DataFrame
+        DataFrame containing a scenario with 'latitude', 'longitude',
+        'heading', and 'callsign' columns
+    output_file : str
+        Output file for html (if None, use bokeh default)
+    """
+    if output_file is not None:
+        bkplot.output_file(output_file)
+
+    p = bkplot.figure(x_axis_type='mercator', y_axis_type='mercator')
+    tile_provider = get_provider(Vendors.CARTODBPOSITRON)
+    p.add_tile(tile_provider)
+
+    df = df_in[['latitude','longitude','heading','callsign']].copy()
+    df['longitude'], df['latitude'] = merc(df['latitude'].values, df['longitude'].values)
+
+    points = p.triangle(x='longitude', y='latitude', angle='heading', angle_units='deg', alpha=0.5, source=df)
+
+    callsigns = df['callsign'].unique()
+    points.glyph.fill_color = bk.transform.factor_cmap('callsign', palette=bk.palettes.Category10[10], factors=callsigns)
+    points.glyph.line_color = bk.transform.factor_cmap('callsign', palette=bk.palettes.Category10[10], factors=callsigns)
+
+    bkplot.show(p)
