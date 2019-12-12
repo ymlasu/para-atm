839acc12d03c870238a23d62ef6c2670ecc6ee6a
John McFarland <john.mcfarland@swri.org>
2019-12-12 15:41:14 -0600

    Adding new read function read_iff_file
    This is to replace the current code under Commands/readIFF.py.  The
    changes are intended to simplify the code, speed it up, make it more
    robust, and improve it to read any and all record types.
    
    The new approach works by first reading the entire file in as a list
    of lines, identifying the record type associated with each line, and
    then reading the collection of lines for each record type separately
    into a DataFrame.  Even for large ~150MB files, this takes only a few
    seconds, so the added complexity of parallel processing is no longer
    necessary.
    
    A mapping of record types -> column labels is defined for a base IFF
    format 2.6.  In some cases, newer versions add additional columns.
    Using the old column specification would still work (and the new
    columns would not be read), but some logic has been added to try and
    account for some of the changes in versions 2.13 and 2.15, so that the
    new columns are processed as well.
    
    The read_iff_file function allows the user to request a single record
    type, a list of record types, or 'all'.  The result will either be a
    single DataFrame or a dictionary mapping record types to DataFrames.
    
    The interpolation option has not been included (and it does not appear
    that the current interpolation in readIFF.py is operational anyway).
    Thinking that it may be better to place interpolation into a utility
    function that can be shared across the package and that operates
    separately from the read functions.
    
    This version does not add a "status" column like the previous
    version.  Suggest that if needed, utility functions could be written
    to compute a status and provide more control over the specifics.
    Prefer to limit the reader code to just reading.

---
+++ src/PARA_ATM/io/iff.py
@@ -0,0 +1,109 @@
+"""Functions for reading Integrated Flight Format (IFF) files"""
+
+import pandas as pd
+import numpy as np
+import io
+
+def read_iff_file(filename, record_types='all', interp=False):
+    """
+    Parameters
+    ----------
+    filename : str
+    record_types : int, sequence of ints, or 'all'
+        Record types to return
+    
+    Returns
+    -------
+    DataFrame or dict of DataFrames
+       If record_types is a scalar, return a DataFrame containing the
+       data for that record type only.  Otherwise, return a dictionary
+       mapping each requested record type to a corresponding DataFrame.
+    """
+
+    # Determine file format version.  This is in record type 1, which
+    # for now we assume to occur on the first line.
+    with open(filename, 'r') as f:
+        version = float(f.readline().split(',')[2])
+
+    # Columns for each record type, from version 2.6 specification.
+    cols = {0:['recType','comment'],
+            1:['recType','fileType','fileFormatVersion'],
+            2:['recType','recTime','fltKey','bcnCode','cid','Source','msgType','AcId','recTypeCat','acType','Orig','Dest','opsType','estOrig','estDest'],
+            3:['recType','recTime','fltKey','bcnCode','cid','Source','msgType','AcId','recTypeCat','coord1','coord2','alt','significance','coord1Accur','coord2Accur','altAccur','groundSpeed','course','rateOfClimb','altQualifier','altIndicator','trackPtStatus','leaderDir','scratchPad','msawInhibitInd','assignedAltString','controllingFac','controllingSeg','receivingFac','receivingSec','activeContr','primaryContr','kybrdSubset','kybrdSymbol','adsCode','opsType','airportCode'],
+            4:['recType','recTime','fltKey','bcnCode','cid','Source','msgType','AcId','recTypeCat','acType','Orig','Dest','altcode','alt','maxAlt','assignedAltString','requestedAltString','route','estTime','fltCat','perfCat','opsType','equipList','coordinationTime','coordinationTimeType','leaderDir','scratchPad1','scratchPad2','fixPairScratchPad','prefDepArrRoute','prefDepRoute','prefArrRoute'],
+            5:['recType','dataSource','programName','programVersion'],
+            6:['recType','recTime','Source','msgType','rectypeCat','sectorizationString'],
+            7:['recType','recTime','fltKey','bcnCode','cid','Source','msgType','AcId','recTypeCat','coord1','coord2','alt','significance','coord1Accur','coord2Accur','altAccur','msawtype','msawTimeCat','msawLocCat','msawMinSafeAlt','msawIndex1','msawIndex2','msawVolID'],
+            8:['recType','recTime','fltKey','bcnCode','cid','Source','msgType','AcId','recTypeCat','acType','Orig','Dest','depTime','depTimeType','arrTime','arrTimeType'],
+            9:['recType','recTime','fltKey','bcnCode','cid','Source','msgType','AcId','recTypeCat','coord1','coord2','alt','pitchAngle','trueHeading','rollAngle','trueAirSpeed','fltPhaseIndicator'],
+            10:['recType','recTime','fltKey','bcnCode','cid','Source','msgType','AcId','recTypeCat','configType','configSpec']}
+
+    # For newer versions, additional columns are supported.  However,
+    # this code could be commented out, and it should still be
+    # compatible with newer versions, but just ignoring the additional
+    # columns.
+    EPS = 1e-7 # Avoid rounding issues when comparing versions
+    if version >= 2.13 + EPS:
+        cols[2] += ['modeSCode']
+        cols[3] += ['trackNumber','tptReturnType','modeSCode']
+        cols[4] += ['coordinationPoint','coordinationPointType','trackNumber','modeSCode']
+    elif version >= 2.15 + EPS:
+        cols[3] += ['sensorTrackNumberList','spi','dvs','dupM3a','tid']
+
+        
+    # Read entire file and store as an array of lines:
+    with open(filename, 'r') as f:
+        data = np.array(f.readlines())
+
+    # Create an array of the record types:
+    line_record_types = np.array([int(line.split(',')[0]) for line in data])
+
+    #print('available record types:', np.unique(line_record_types))
+
+    # Determine which record types to retrieve, and whether the result
+    # should be a scalar or dict:
+    if record_types == 'all':
+        record_types = np.unique(line_record_types)
+        scalar_result = False
+    elif hasattr(record_types, '__getitem__'):
+        scalar_result = False
+    else:
+        scalar_result = True
+        record_types = [record_types]
+
+    # For each record type, read the corresponding lines into a pandas
+    # DataFrame:
+    data_frames = dict()
+    for record_type in record_types:
+        lines = data[line_record_types == record_type]
+        string_data = io.StringIO( '\n'.join(lines) )
+        # Passing usecols is necessary because for some records, the
+        # actual data has extraneous empty columns at the end, in
+        # which case the data does not seem to get read correctly
+        # without usecols
+        df = pd.read_csv(string_data, header=None, names=cols[record_type], low_memory=False, usecols=cols[record_type], na_values='?')
+
+        # For consistency with other PARA-ATM data:
+        df.rename(columns={'recTime':'time',
+                           'AcId':'callsign',
+                           'coord1':'latitude',
+                           'coord2':'longitude',
+                           'alt':'altitude',
+                           'rateOfClimb':'rocd',
+                           'groundSpeed':'tas',
+                           'course':'heading'},
+                  inplace=True)
+
+        if 'time' in df:
+            df['time'] = pd.to_datetime(df['time'], unit='s')
+
+        # Store to dict of data frames
+        data_frames[record_type] = df
+
+    if scalar_result:
+        result = data_frames[record_types[0]]
+    else:
+        result = data_frames
+
+    return result
+    
