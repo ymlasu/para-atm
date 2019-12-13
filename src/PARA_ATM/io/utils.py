5b0a69fa106717c87210202982a2d63e692750ee
John McFarland <john.mcfarland@swri.org>
2019-12-13 14:23:32 -0600

    Add utility functions read_csv_file and write_csv_file
    These are simple but provide consistent settings when calling the
    pandas read/write functions and make sure the time column is converted
    to datetime.

--- src/PARA_ATM/io/utils.py
+++ src/PARA_ATM/io/utils.py
@@ -1,6 +1,7 @@
 """Utility functions for file input/output"""
 
 from enum import Enum, auto
+import pandas as pd
 
 from . import nats
 from . import iff
@@ -10,6 +11,7 @@ class FileTypes(Enum):
     UNKNOWN = auto()
     NATS = auto()
     IFF = auto()
+    CSV = auto()
 
 def detect_data_file_type(filename):
     """Try and detect the format of a given data file
@@ -24,8 +26,20 @@ def detect_data_file_type(filename):
         return FileTypes.NATS
     elif 'IFF' in line:
         return FileTypes.IFF
+    elif ',' in line:
+        # Assume that it is a regular CSV file
+        return FileTypes.CSV
     else:
         return FileTypes.UNKNOWN
+
+def read_csv_file(filename):
+    """Read a CSV file previously created by PARA_ATM"""
+    df = pd.read_csv(filename, converters={'time':pd.to_datetime})
+    return df
+
+def write_csv_file(df, filename):
+    """Write a DataFrame to csv"""
+    df.to_csv(filename, index=False)
 
 def read_data_file(filename, *args, **kwargs):
     """Detect file format and read scenario data
@@ -42,5 +56,7 @@ def read_data_file(filename, *args, **kwargs):
         return nats.read_nats_output_file(filename, *args, **kwargs)
     elif filetype == FileTypes.IFF:
         return iff.read_iff_file(filename, *args, **kwargs)
+    elif filetype == FileTypes.CSV:
+        return read_csv_file(filename)
     else:
         raise ValueError('unrecognized file type')
