b4823202d6a4fc9913189ecd9c1b54d348a22dd3
John McFarland <john.mcfarland@swri.org>
2020-02-05 16:57:09 -0600

    Minor cleanup to setup.py project data
    Change version number, fix name, remove url, author, and classifiers.

--- setup.py
+++ setup.py
@@ -2,18 +2,9 @@ from setuptools import setup, find_packages
 from os import path
 
 setup(
-        name='PARAATM',
-        version='1.0',
+        name='PARA-ATM',
+        version='0.1',
         description='Probabilistic toolset for air traffic safety analysis',
-        url='https://github.com/mh-swri/NASA_ULI_InfoFusion',
-        author='Southwest Research Institute',
-        classifiers=[
-            'Development Status :: 3 - Alpha',
-            'Intended Audience :: ATM researchers',
-            'Topic :: NAS Safety',
-            'License :: Open',
-            ],
-        #package_dir={'': 'src/PARA_ATM'},
         packages=find_packages(),
         install_requires=[
             'pandas',
