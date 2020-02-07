2527acb437a7d505345f733fafbd8cc7f6c5b747
John McFarland <john.mcfarland@swri.org>
2020-02-07 16:25:45 -0600

    Deactivating App and DB test cases
    Both depend on the database, which isn't currently distributed or
    configured.  These test cases might be completely removed or reworked
    once a firmer plan is made for the database and GUI app.

--- paraatm/tests/test_para_atm.py
+++ paraatm/tests/test_para_atm.py
@@ -3,8 +3,8 @@ import pandas as pd
 import numpy as np
 import os
 
-from paraatm.Commands.Helpers.DataStore import Access
-from paraatm.Application import LaunchApp
+# from paraatm.Commands.Helpers.DataStore import Access
+# from paraatm.Application import LaunchApp
 from paraatm.io.nats import read_nats_output_file, NatsEnvironment
 from paraatm.io.iff import read_iff_file
 from paraatm.io.utils import read_csv_file
@@ -15,72 +15,76 @@ from .nats_gate_to_gate import GateToGate
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 sample_nats_file = os.path.join(THIS_DIR, '..', 'sample_data/NATS_output_SFO_PHX.csv')
 
-class TestApp(unittest.TestCase):
-    def setUp(self):
-        self.container = LaunchApp.Container()
-        self.db_access = Access()
+# Deactivating App and DB test cases for the time being, as they both
+# depend on the database functionality, which is currently not
+# configured.
 
-    def tearDown(self):
-        # Ensure table is removed even if test fails (safe to call on non-existent table)
-        self.db_access.dropTable('test_table')
+# class TestApp(unittest.TestCase):
+#     def setUp(self):
+#         self.container = LaunchApp.Container()
+#         self.db_access = Access()
+
+#     def tearDown(self):
+#         # Ensure table is removed even if test fails (safe to call on non-existent table)
+#         self.db_access.dropTable('test_table')
     
-    def test_construction(self):
-        """Test construction of LaunchApp container"""
-        # Verify that the Container constructor runs without error.
-        # In future, could add assertions that check specific
-        # attributes of the result (e.g. that some of the
-        # widgets/tables have certain characteristics), but for now
-        # the test is soley used to verify that exceptions do not
-        # occur during creation of the container.
+#     def test_construction(self):
+#         """Test construction of LaunchApp container"""
+#         # Verify that the Container constructor runs without error.
+#         # In future, could add assertions that check specific
+#         # attributes of the result (e.g. that some of the
+#         # widgets/tables have certain characteristics), but for now
+#         # the test is soley used to verify that exceptions do not
+#         # occur during creation of the container.
 
-        # Although there is no code here, this is kept as a separate
-        # test to differentiate between errors in container creation
-        # that happen in setUp, versus errors in functions tested via
-        # other test cases.
-        pass
+#         # Although there is no code here, this is kept as a separate
+#         # test to differentiate between errors in container creation
+#         # that happen in setUp, versus errors in functions tested via
+#         # other test cases.
+#         pass
 
-    def testTableSelect(self):
-        """Test the callback when the table selection changes"""
+#     def testTableSelect(self):
+#         """Test the callback when the table selection changes"""
         
-        # First we need to ensure there is a valid table in the
-        # database, so we do this by reading in a sample NATS output
-        # file and adding it to the database
-        df = read_nats_output_file(sample_nats_file)
-        self.db_access.addTable('test_table', df)
+#         # First we need to ensure there is a valid table in the
+#         # database, so we do this by reading in a sample NATS output
+#         # file and adding it to the database
+#         df = read_nats_output_file(sample_nats_file)
+#         self.db_access.addTable('test_table', df)
 
-        # Try and select the new table
-        self.container.tables.value = 'test_table'
+#         # Try and select the new table
+#         self.container.tables.value = 'test_table'
 
 
-class TestDB(unittest.TestCase):
-    def setUp(self):
-        self.db_access = Access()
+# class TestDB(unittest.TestCase):
+#     def setUp(self):
+#         self.db_access = Access()
 
-    def tearDown(self):
-        # Ensure table is removed even if test fails (safe to call on non-existent table)
-        self.db_access.dropTable('test_table')        
+#     def tearDown(self):
+#         # Ensure table is removed even if test fails (safe to call on non-existent table)
+#         self.db_access.dropTable('test_table')        
 
-    def testTableNotExists(self):
-        self.assertFalse(self.db_access.tableExists('nonexistent_table'))
+#     def testTableNotExists(self):
+#         self.assertFalse(self.db_access.tableExists('nonexistent_table'))
 
-    def testAddReadDrop(self):
-        """Add a table, read it, then drop it"""
-        # Add a table
-        df1 = pd.DataFrame(np.eye(3))
-        self.db_access.addTable('test_table', df1, index=False)
+#     def testAddReadDrop(self):
+#         """Add a table, read it, then drop it"""
+#         # Add a table
+#         df1 = pd.DataFrame(np.eye(3))
+#         self.db_access.addTable('test_table', df1, index=False)
 
-        # Verify that it exists
-        self.assertTrue(self.db_access.tableExists('test_table'))
+#         # Verify that it exists
+#         self.assertTrue(self.db_access.tableExists('test_table'))
 
-        # Read it back
-        df2 = self.db_access.readTable('test_table')
-        self.assertTrue(np.all(df1.values==df2.values))
+#         # Read it back
+#         df2 = self.db_access.readTable('test_table')
+#         self.assertTrue(np.all(df1.values==df2.values))
 
-        # Drop it
-        self.db_access.dropTable('test_table')
+#         # Drop it
+#         self.db_access.dropTable('test_table')
 
-        # Verify that it doesn't exist
-        self.assertFalse(self.db_access.tableExists('test_table'))
+#         # Verify that it doesn't exist
+#         self.assertFalse(self.db_access.tableExists('test_table'))
 
 class TestNATSFiles(unittest.TestCase):
     def test_read_nats_output(self):
