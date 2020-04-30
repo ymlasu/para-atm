9b3d33e412e158902d35562592fdcd0de7666550
John McFarland <john.mcfarland@swri.org>
2020-04-30 10:18:16 -0500

    Add gnats command to para-atm command line utility

--- paraatm/paraatm.py
+++ paraatm/paraatm.py
@@ -11,6 +11,7 @@ import importlib
 from paraatm.io.utils import read_data_file
 from paraatm.plotting import plot_trajectory
 from paraatm.io.nats import NatsSimulationWrapper
+from paraatm.io.gnats import GnatsSimulationWrapper
 
 def main():
 
@@ -29,6 +30,11 @@ def main():
     p_nats.add_argument('--output', help='file to store nats results')
     p_nats.add_argument('--plot', action='store_true', help='plot results')
 
+    p_gnats = subparsers.add_parser('gnats', help='run GNATS simulation implemented via GnatsSimulationWrapper')
+    p_gnats.add_argument('file', help='python module with a class that derives from GnatsSimulationWrapper')
+    p_gnats.add_argument('--output', help='file to store gnats results')
+    p_gnats.add_argument('--plot', action='store_true', help='plot results')
+
 
     args = parser.parse_args()
 
@@ -39,17 +45,22 @@ def main():
         df = read_data_file(args.file)
         plot_trajectory(df)
         
-    elif args.command == 'nats':
+    elif args.command == 'nats' or args.command == 'gnats':
         dirname = os.path.dirname(os.path.abspath(args.file))
         sys.path.insert(0, dirname)
         module = importlib.import_module(os.path.basename(args.file).replace('.py',''))
         # Find appropriate classes in the user-specified module:
-        classes = inspect.getmembers(module, lambda member: inspect.isclass(member) and issubclass(member, NatsSimulationWrapper) and member is not NatsSimulationWrapper)
-        if len(classes) < 1:
-            raise ValueError('no subclass of NatsSimulationWrapper found in {}'.format(args.file))
+        if args.command == 'nats':
+            classes = inspect.getmembers(module, lambda member: inspect.isclass(member) and issubclass(member, NatsSimulationWrapper) and member is not NatsSimulationWrapper)
+            if len(classes) < 1:
+                raise ValueError('no subclass of NatsSimulationWrapper found in {}'.format(args.file))
+        else:
+            classes = inspect.getmembers(module, lambda member: inspect.isclass(member) and issubclass(member, GnatsSimulationWrapper) and member is not GnatsSimulationWrapper)            
+            if len(classes) < 1:
+                raise ValueError('no subclass of GnatsSimulationWrapper found in {}'.format(args.file))
         class_name, the_class = classes[0] # Use the first available class
-        print('Creating NATS simulation from: {}'.format(class_name))
-        nats = the_class() # Instatiate class
-        df = nats(output_file=args.output) # Run simulation
+        print('Creating simulation from: {}'.format(class_name))
+        sim = the_class() # Instatiate class
+        df = sim(output_file=args.output) # Run simulation
         if args.plot:
             plot_trajectory(df)
