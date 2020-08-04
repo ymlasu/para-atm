042489cf532c23a74a4fcef165d81afac0a5199d
John McFarland <john.mcfarland@swri.org>
2020-08-04 14:12:10 -0500

    Simplify read_gnats_output_file to keep all columns
    Just return the data for all columns instead of selecting out specific
    ones.  This also fixes an error that can occur if trying to use
    read_gnats_output_file to read a NATS output file, e.g. the sample
    file NATS_demo_5_aircraft.csv (which can happen when using the command
    line interface "para-atm plot").

--- paraatm/io/gnats.py
+++ paraatm/io/gnats.py
@@ -386,11 +386,5 @@ def read_gnats_output_file(filename):
     df['time'] += start_time
     df['time'] = pd.to_datetime(df['time'], unit='s')
 
-    # Keep only selected columns
-    selected_cols = ['time','callsign','origin','destination','latitude','longitude','altitude','rocd','tas','heading','status']
-    if 'sector' in df:
-        selected_cols += 'sector'
-    df = df[selected_cols]
-
     return df
 
