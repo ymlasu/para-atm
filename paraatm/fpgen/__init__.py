932823168319275468ec470df6991d00cb983de5
edecarlo <erin.decarlo@swri.org>
2021-04-09 17:26:44 -0500

    Fix runway selection from iff data

--- paraatm/fpgen/__init__.py
+++ paraatm/fpgen/__init__.py
@@ -7,15 +7,18 @@ example of file organization.
 """
 
 from ._nats_functions import (
-    get_gate_lat_lon_from_nats
+    get_gate_lat_lon_from_nats,
+    get_random_gate,
+    get_random_runway
 )
 
 from ._iff_functions import (
     get_departure_airport_from_iff,
     get_arrival_airport_from_iff,
-    get_departure_gate_and_rwy_from_iff,
-    get_arrival_gate_and_rwy_from_iff,
-    random_airport_gate_and_rwy,
-    check_if_flight_has_departed,
-    check_if_flight_landing_in_dataset
+    get_gate_from_iff,
+    get_rwy_from_iff,
+    check_if_flight_has_departed_from_iff,
+    check_if_flight_landing_from_iff
 )
+
+from .FlightPlanSelector import FlightPlanSelector
--- paraatm/fpgen/_nats_functions.py
+++ paraatm/fpgen/_nats_functions.py
@@ -2,6 +2,25 @@ import os
 
 GNATS_HOME = os.environ.get('NATS_HOME')
 
+def get_random_gate(natsSim,airport):
+    import random
+    
+    if len(airport)==3: airport = 'K'+airport
+
+    gateOptions =list(natsSim.airportInterface.getAllGates(airport))
+    gateOptions = [opt for opt in gateOptions if opt.lower().startswith('gate')]
+    return random.choice(gateOptions)
+
+def get_random_runway(natsSim,airport,arrival=True):
+    import random
+    #Todo: Figure out how to build in arriva/departure logic
+    if len(airport)==3: airport = 'K'+airport
+    
+    usable_apts_and_rwys = get_usable_apts_and_rwys(natsSim)
+    rwyOptions = usable_apts_and_rwys[airport]
+
+    return  random.choice(rwyOptions)
+
 def get_gate_lat_lon_from_nats(natsSim,gate,airport):
     import pandas as pd
     df = pd.read_csv(GNATS_HOME+'/../GNATS_Server/share/libairport_layout/Airport_Rwy/{}_Nodes_Def.csv'.format(airport))
@@ -9,49 +28,59 @@ def get_gate_lat_lon_from_nats(natsSim,gate,airport):
     lon = df.loc[df['id']==gate]['lon'].values[0]
     return lat,lon
 
-def get_usable_apts_and_rwys(natsSim):
+def get_usable_apts_and_rwys(natsSim,arrival=True):
     apts=list(natsSim.airportInterface.getAllAirportCodesInGNATS())
     usable_apts_and_rwys = {}
-    dumbAirports = ['KMEM','KSNA']
+    unusableAirports = ['KMEM','KSNA']
     for apt in apts:
-        if apt[0]=='K' and apt not in dumbAirports:
+        if apt[0]=='K' and apt not in unusableAirports:
             rwys = list(natsSim.airportInterface.getAllRunways(apt))
             rwys= [list(rwy) for rwy in rwys]
             rwy_nodes = [rwy[1] for rwy in rwys]
             rwys = [rwy[0] for rwy in rwys]
             approachProcedures = natsSim.terminalAreaInterface.getAllApproaches(apt)
-            usableAPs = [ap[1:] for ap in approachProcedures if ap.startswith("I")]
 
-            usableRws = []
-            for rwy_node in rwy_nodes:
-                rwy_entry,rwy_end=get_landing_rwy_entry_and_end_point(rwy_node,apt,domain=['Rwy'])
-                if rwy_entry[2:] in usableAPs:
+            if arrival:
+                usableAPs = [ap[1:] for ap in approachProcedures]
+                usableRws = []
+                for rwy_node in rwy_nodes:
+                    rwy_entry,rwy_end=get_rwy_entry_and_end_point(rwy_node,apt)
+                    if rwy_entry[2:] in usableAPs:
+                        usableRws.append(rwy_entry)
+                if usableRws:
+                    usable_apts_and_rwys.update({apt : usableRws})
+
+            if not arrival:
+                usableRws = []
+                for rwy_node in rwy_nodes:
+                    rwy_entry,rwy_end=get_rwy_entry_and_end_point(rwy_node,apt)
                     usableRws.append(rwy_entry)
-            if usableRws:
-                usable_apts_and_rwys.update({apt : usableRws})
+                if usableRws:
+                    usable_apts_and_rwys.update({apt : usableRws})
+            
     return usable_apts_and_rwys
 
-def get_landing_rwy_entry_and_end_point(landing_rwy_node,airport,domain=['Rwy']):
+def get_rwy_entry_and_end_point(rwy_node,airport):
     import pandas as pd
     import numpy as np
     import random
     
     df = pd.read_csv(GNATS_HOME+'/../GNATS_Server/share/libairport_layout/Airport_Rwy/{}_Nodes_Def.csv'.format(airport))
-    df = df.loc[df.domain.isin(domain)]
+    df = df.loc[df.domain.isin(['Rwy'])]
     df_subs = df.dropna(axis=0,how='any')
     df_subs_id = df_subs.id.values.tolist()
 
-    if np.any(df_subs.id==landing_rwy_node):
-        df_subs_landing = df_subs[df_subs.id==landing_rwy_node]
+    if np.any(df_subs.id==rwy_node):
+        df_subs_landing = df_subs[df_subs.id==rwy_node]
         entry_point = df_subs_landing.refName1.values[0]
         end_point = df_subs_landing.refName2.values[0]
-    elif np.any([i.startswith(landing_rwy_node[:6]) for i in df_subs_id]):
-        df_subs_landing = df_subs.loc[[i.startswith(landing_rwy_node[:6]) for i in df_subs_id],:]
+    elif np.any([i.startswith(rwy_node[:6]) for i in df_subs_id]):
+        df_subs_landing = df_subs.loc[[i.startswith(rwy_node[:6]) for i in df_subs_id],:]
         entry_point = df_subs_landing.refName1.values[0]
         end_point = df_subs_landing.refName2.values[0]
     else:
         landing_rwy_node = random.choice(df_subs.id.values)
-        df_subs_landing = df_subs[df_subs.id==landing_rwy_node]
+        df_subs_landing = df_subs[df_subs.id==rwy_node]
         entry_point = df_subs_landing.refName1.values[0]
         end_point = df_subs_landing.refName2.values[0]
     
@@ -87,13 +116,13 @@ def get_adjacent_node_closer_to_runway(nodeList,runwayNode,airport,removed_nodes
     closest_node = df.loc[df.dists.idxmin()]['id']
     return closest_node
 
-def get_closest_airport(gnatsSim,lat,lon,asdex_apt):
+def get_closest_airport(natsSim,lat,lon,asdex_apt):
     import pandas as pd
     import numpy as np
 
-    candApts = list(gnatsSim.airportInterface.getAirportsWithinMiles(lat,lon,100))
+    candApts = list(natsSim.airportInterface.getAirportsWithinMiles(lat,lon,100))
     candApts = [apt for apt in candApts if apt.startswith('K') and asdex_apt not in apt]
-    lats_lons = [list(gnatsSim.airportInterface.getLocation(apt)) for apt in candApts]
+    lats_lons = [list(natsSim.airportInterface.getLocation(apt)) for apt in candApts]
     dists = [np.sqrt((entry[0]-lat)**2+(entry[1]-lon)**2) for entry in lats_lons]
     closest_apt = candApts[np.argmin(dists)]
     return closest_apt
