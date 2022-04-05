11f9587a953063addc4d48276343a2a81e634afa
edecarlo-swri <erin.decarlo@swri.org>
2022-04-05 15:18:54 -0500

    added logisitic regression pickle file

---
+++ paraatm/safety/los.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+from catboost import CatBoostClassifier
+from sklearn.linear_model import LogisticRegression
+import pickle
+
+class LosModel():
+    def __init__(self,model_name,model_type='catboost'):
+        self.model_type=model_type
+
+        if self.model_type == 'catboost':
+            self.model = CatBoostClassifier()
+            self.model.load_model(model_name)
+
+        if self.model_type == 'LogisticRegression':
+            self.model = pickle.load(open(model_name,'rb'))
+
+
+    def __call__(self,x):
+        if self.model_type == 'catboost':
+            y=self.model.predict(x)
+
+        if self.model_type == 'LogisticRegression':
+            y = self.model.predict(x)
+        
+        return(y)
+        
